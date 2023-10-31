ba6646f01a5b913bae83c5e157ed3034e1d3f532
YeiHuynh <YeiHuynh>
2023-10-31 22:44:18 +0700

    Fix color

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ let currentQuoteIndex = 0;
 function displayQuote() {
   const currentQuote = quotes[currentQuoteIndex];
   quoteElement.textContent = currentQuote.quote;
-  authorElement.innerHTML = `by : <a href="${currentQuote.profilLink}" target="_blank">${currentQuote.name}</a>`;
+  authorElement.innerHTML = `by : <a href="${currentQuote.profilLink}" target="_blank" style="color:rgb(5, 110, 28);">${currentQuote.name}</a>`;
   // currentQuoteIndex = Math.floor(Math.random() * quotes.length)
-  currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length ;
+  currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length;
 }
 
 // Initial display

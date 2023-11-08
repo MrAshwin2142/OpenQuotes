f6fba1d12e24ffd8a498bb86a0c64f73a2fc9efb
ngoc1230321 <huynhtrantuanngoc2002@gmail.com>
2023-11-08 21:56:25 +0700

    Added my inspirational quote

--- quotes.js
+++ quotes.js
@@ -1,91 +1,111 @@
 const quotes = [
-    {
-        quote: "You can kickstart your open-source contribution journey by simply adding a quote or sharing anything you'd like on this page.",
-        name: "Ashwin Dhangar",
-        profilLink: "https://github.com/MrAshwin2142",
-    },
-    {
-        quote:"Stop dreaming big and start with small actions, just like I did for my first open-source contribution.",
-        name: "Code Gallery",
-        profilLink: "https://www.linkedin.com/company/code-gallery01/",
-    },
-    {
-        quote:"कर्म करो और फल की चिंता मत करो - Lord Krishna",
-        name: "Ashwin Dhangar",
-        profilLink: "https://github.com/MrAshwin2142",
-    },
-    {   quote:"Just trying to learn how to use Git and Github properly by contributing.",
-        name: "Vivek Mandloi",
-        profilLink: "https://github.com/VveksGit/",
-    },
-    {   quote:"The stars above remind us that even the smallest lights can illuminate the darkest skies.",
-        name: "John Patrick Ancajas",
-        profilLink: "https://github.com/JohnPatrickAncajas",
-    },
-    {   quote:"Learn as if you will live forever, live like you will die tomorrow. — Mahatma Gandhi",
-        name:"Delta7Actual",
-        profilLink:"https://github.com/Delta7Actual",
-    },
-    {   
-        quote:"It is often the small steps, not the giant leaps, that bring about the most lasting change. - Elizabeth",
-        name:"Umar Farouk",
-        profilLink:"https://github.com/Fahass",
-    },
-    {   
-        quote:"Failure is your first reward for trying. - Ameer Fahass",
-        name:"Umar Farouk",
-        profilLink:"https://github.com/Fahass",
-    },
-    {   
-        quote:"If you want to lift yourself up, lift up someone else. - Chadwick Bossman",
-        name:"Umar Farouk",
-        profilLink:"https://github.com/Fahass",
-    },
-    {   
-        quote:"You’re braver than you believe, stronger than you seem, and smarter than you think. - Ameer Fahass",
-        name:"Umar Farouk",
-        profilLink:"https://github.com/Fahass",
-    },
-    {   
-        quote:"Once you face your fear, nothing is ever as hard as you think. - Ameer Fahass",
-        name:"Umar Farouk",
-        profilLink:"https://github.com/Fahass",
-    },
-    {
-        quote: "I expressed my complaints through a story on fb, insta,... and hoped that the issue would also disappear within 24 hours.",
-        name: "vtkmy",
-        profilLink: "https://github.com/vtkmy",
-    },
-    {
-        quote: "When we strive to become better than we are, everything around us becomes better too. — Paulo Coelho",
-        name: "Nimish Toshniwal",
-        profilLink: "https://github.com/NimishToshniwal",
-    },
-    {
-        quote: "Everything has beauty, but not everyone sees it.",
-        name: "Tran Vinh Hung",
-        githubLink: "https://github.com/vinhhung0306",
-    },
-    {
-        quote: "Only those who believe their own path will eventually shine until their lives burn out",
-        name: "Kertyn",
-        githubLink: "https://github.com/kertyn1",
-    },
-    {
-        quote: "People say nothing is impossible, but I do nothing every day. — Winnie the Pooh",
-        name: "Kertyn",
-        githubLink: "https://github.com/kertyn1",
-    },
-    {
-        quote: "Life is Roblox, don't let it stop you! — DJ Khaled",
-        name: "WD_master235",
-        githubLink: "https://github.com/WDmaster235",
-    },
-    
-    // Add more objects with quotes, names, and Profile  links
-    // {
-    //     quote: "your quote or anything you want to share",
-    //     name: "your name",
-    //     profilLink: "https://github.com/your-github",
-    // },
+  {
+    quote:
+      "You can kickstart your open-source contribution journey by simply adding a quote or sharing anything you'd like on this page.",
+    name: "Ashwin Dhangar",
+    profilLink: "https://github.com/MrAshwin2142",
+  },
+  {
+    quote:
+      "Stop dreaming big and start with small actions, just like I did for my first open-source contribution.",
+    name: "Code Gallery",
+    profilLink: "https://www.linkedin.com/company/code-gallery01/",
+  },
+  {
+    quote: "कर्म करो और फल की चिंता मत करो - Lord Krishna",
+    name: "Ashwin Dhangar",
+    profilLink: "https://github.com/MrAshwin2142",
+  },
+  {
+    quote:
+      "Just trying to learn how to use Git and Github properly by contributing.",
+    name: "Vivek Mandloi",
+    profilLink: "https://github.com/VveksGit/",
+  },
+  {
+    quote:
+      "The stars above remind us that even the smallest lights can illuminate the darkest skies.",
+    name: "John Patrick Ancajas",
+    profilLink: "https://github.com/JohnPatrickAncajas",
+  },
+  {
+    quote:
+      "Learn as if you will live forever, live like you will die tomorrow. — Mahatma Gandhi",
+    name: "Delta7Actual",
+    profilLink: "https://github.com/Delta7Actual",
+  },
+  {
+    quote:
+      "It is often the small steps, not the giant leaps, that bring about the most lasting change. - Elizabeth",
+    name: "Umar Farouk",
+    profilLink: "https://github.com/Fahass",
+  },
+  {
+    quote: "Failure is your first reward for trying. - Ameer Fahass",
+    name: "Umar Farouk",
+    profilLink: "https://github.com/Fahass",
+  },
+  {
+    quote:
+      "If you want to lift yourself up, lift up someone else. - Chadwick Bossman",
+    name: "Umar Farouk",
+    profilLink: "https://github.com/Fahass",
+  },
+  {
+    quote:
+      "You’re braver than you believe, stronger than you seem, and smarter than you think. - Ameer Fahass",
+    name: "Umar Farouk",
+    profilLink: "https://github.com/Fahass",
+  },
+  {
+    quote:
+      "Once you face your fear, nothing is ever as hard as you think. - Ameer Fahass",
+    name: "Umar Farouk",
+    profilLink: "https://github.com/Fahass",
+  },
+  {
+    quote:
+      "I expressed my complaints through a story on fb, insta,... and hoped that the issue would also disappear within 24 hours.",
+    name: "vtkmy",
+    profilLink: "https://github.com/vtkmy",
+  },
+  {
+    quote:
+      "When we strive to become better than we are, everything around us becomes better too. — Paulo Coelho",
+    name: "Nimish Toshniwal",
+    profilLink: "https://github.com/NimishToshniwal",
+  },
+  {
+    quote: "Everything has beauty, but not everyone sees it.",
+    name: "Tran Vinh Hung",
+    githubLink: "https://github.com/vinhhung0306",
+  },
+  {
+    quote:
+      "Only those who believe their own path will eventually shine until their lives burn out",
+    name: "Kertyn",
+    githubLink: "https://github.com/kertyn1",
+  },
+  {
+    quote:
+      "People say nothing is impossible, but I do nothing every day. — Winnie the Pooh",
+    name: "Kertyn",
+    githubLink: "https://github.com/kertyn1",
+  },
+  {
+    quote: "Life is Roblox, don't let it stop you! — DJ Khaled",
+    name: "WD_master235",
+    githubLink: "https://github.com/WDmaster235",
+  },
+  {
+    quote: "Be Yourself",
+    name: "Huynh Tran Tuan Ngoc",
+    profilLink: "https://github.com/your-github",
+  },
+  // Add more objects with quotes, names, and Profile  links
+  // {
+  //     quote: "your quote or anything you want to share",
+  //     name: "your name",
+  //     profilLink: "https://github.com/your-github",
+  // },
 ];
